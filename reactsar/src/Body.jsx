477d022f0c1dd1f74f5d2bee07ba98296e98abc5
senaimussie <128145833+senaimussie@users.noreply.github.com>
2024-12-02 12:44:25 +0300

    Your descriptive commit message here

---
+++ reactsar/src/Body.jsx
@@ -0,0 +1,14 @@
+
+
+
+function MainContent() {
+    return (
+      <main className="main-content">
+        <h1>Feature</h1>
+        <hr />
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  
---
+++ reactsar/src/Header.jsx
@@ -0,0 +1,21 @@
+
+
+function Navbar() {
+    return (
+      <header className="navbar">
+        <div className="navbar-logo">
+          
+          <span>Company name</span>
+        </div>
+        <nav className="navbar-links">
+          <a href="#features">Features</a>
+          <a href="#enterprise">Enterprise</a>
+          <a href="#support">Support</a>
+          <a href="#home">Home</a>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Navbar;
+  
